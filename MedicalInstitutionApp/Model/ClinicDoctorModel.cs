aa4dfcd706ae19a4900262c9f9d5c0fdd8e89209
Dalish <Dalish@DESKTOP-S7S9PJT>
2020-08-10 16:39:31 +0200

    Added CRUD methods to class ClinicDoctorModel

--- MedicalInstitutionApp/Model/ClinicDoctorModel.cs
+++ MedicalInstitutionApp/Model/ClinicDoctorModel.cs
@@ -3,10 +3,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MedicalInstitutionApp.Model
 {
     class ClinicDoctorModel
     {
+        public List<vwDoctor> GetAllClinicDoctors()
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    return (from x in context.vwDoctors select x).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        public vwDoctor GetClinicDoctor(int ID)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    return (from x in context.vwDoctors where x.DoctorID == ID select x).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        public void DeleteClinicDoctor(int ID)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    context.vwDoctors.Remove((from x in context.vwDoctors where x.DoctorID == ID select x).FirstOrDefault());
+                    context.SaveChanges();
+                    MessageBox.Show("Action successfull!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void AddClinicDoctor(vwDoctor d)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    context.vwDoctors.Add(d);
+                    context.SaveChanges();
+                    MessageBox.Show("Action successfull!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void UpdateClinicDoctor(vwDoctor updated)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    vwDoctor d = (from x in context.vwDoctors where x.DoctorID == updated.DoctorID select x).FirstOrDefault();
+                    d = updated;
+                    context.SaveChanges();
+                    MessageBox.Show("Action successfull!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }

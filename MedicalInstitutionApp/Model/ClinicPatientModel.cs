4169eb18d3d3265cffe78488952200e791d7f3aa
Dalish <Dalish@DESKTOP-S7S9PJT>
2020-08-10 16:49:25 +0200

    Added CRUD methods to ClinicPatientModel

--- MedicalInstitutionApp/Model/ClinicPatientModel.cs
+++ MedicalInstitutionApp/Model/ClinicPatientModel.cs
@@ -3,10 +3,94 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MedicalInstitutionApp.Model
 {
     class ClinicPatientModel
     {
+        public List<vwPatient> GetAllClinicPatients()
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    return (from x in context.vwPatients select x).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        public vwPatient GetClinicPatient(int ID)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    return (from x in context.vwPatients where x.PatientID == ID select x).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        public void DeleteClinicPatient(int ID)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    context.vwPatients.Remove((from x in context.vwPatients where x.PatientID == ID select x).FirstOrDefault());
+                    context.SaveChanges();
+                    MessageBox.Show("Action successfull!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void AddClinicPatient(vwPatient p)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    context.vwPatients.Add(p);
+                    context.SaveChanges();
+                    MessageBox.Show("Action successfull!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void UpdateClinicPatient(vwPatient updated)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    vwPatient p = (from x in context.vwPatients where x.PatientID == updated.PatientID select x).FirstOrDefault();
+                    p = updated;
+                    context.SaveChanges();
+                    MessageBox.Show("Action successfull!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }

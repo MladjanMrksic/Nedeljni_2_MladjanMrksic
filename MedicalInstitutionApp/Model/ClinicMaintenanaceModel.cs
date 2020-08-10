57ab06c1b536892b0961d0c5a3cc43c3dc45a14e
Dalish <Dalish@DESKTOP-S7S9PJT>
2020-08-10 16:24:04 +0200

    Added SQL DDL script, added necessary folders, Added CRUD methods for ClinicMaintenanace

---
+++ MedicalInstitutionApp/Model/ClinicMaintenanaceModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MedicalInstitutionApp.Model
+{
+    class ClinicMaintenanaceModel
+    {
+        public List<vwMaintenance> GetAllClinicMaintenance()
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    return (from x in context.vwMaintenances select x).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        public vwMaintenance GetClinicMaintenance(int ID)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    return (from x in context.vwMaintenances where x.MaintenanaceID == ID select x).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return null;
+            }
+        }
+
+        public void DeleteClinicMaintenance(int ID)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    context.vwMaintenances.Remove((from x in context.vwMaintenances where x.MaintenanaceID == ID select x).FirstOrDefault());
+                    context.SaveChanges();
+                    MessageBox.Show("Action successfull!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void AddClinicMaintenanace(vwMaintenance m)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    context.vwMaintenances.Add(m);
+                    context.SaveChanges();
+                    MessageBox.Show("Action successfull!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void UpdateClinicMaintenance(vwMaintenance updated)
+        {
+            try
+            {
+                using (MedicalInstitutionDatabaseEntities context = new MedicalInstitutionDatabaseEntities())
+                {
+                    vwMaintenance m = (from x in context.vwMaintenances where x.MaintenanaceID == updated.MaintenanaceID select x).FirstOrDefault();
+                    m = updated;
+                    context.SaveChanges();
+                    MessageBox.Show("Action successfull!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception " + ex.Message.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
